0741d7d030d5202126c01ad82479a6c50385c2bb
Seo-051 <kingdom5437@naver.com>
2025-09-09 17:47:18 +0900

    feat 20250909 python test day

--- practice/main.py
+++ practice/main.py
@@ -1,16 +1,20 @@
 game_candidate = int(input('후보 게임 수를 입력하세요 >>> '))
 
-game_list = ' '
+game_list = []
 
-dict = {
+votes_dict = {
 
 }
 
 for i in range(game_candidate) :
-    game_list = input(f'{i+1}번째 게임 이름을 입력하세요 >>> ')
+    game_name = input(f'{i+1}번째 게임 이름을 입력하세요 >>> ')
+    game_list.append(game_name)
 
 pick = input('전체 투표 수를 입력하세요 >>> ')
+game_options = ', '.join([f'{i+1}: {name}' for i, name in enumerate(game_list)])
+print(game_options)
 
-for key, value in range(dict) :
-    print(key, value)
+for key, value in votes_dict.items():
+        print(key, value)
+
 
